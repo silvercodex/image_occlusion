f14b1c4b3db6530284071b016d7a521d56901c26
cody.harmon.yx15@statefarm.com <yx15@mpwd39gd.opr.statefarm.org>
2019-11-06 16:28:59 -0600

    more functions

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import os
 from image_occlusion import *
 
 
+lk_params = dict( winSize  = (15,15),
+                  maxLevel = 2,
+                  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+feature_params = dict( maxCorners = 100,
+                       qualityLevel = 0.3,
+                       minDistance = 7,
+                       blockSize = 7 )
+
+p0 = cv2.goodFeaturesToTrack(gray0, mask = None, **feature_params)
 def load_images(dir = ""):
     files = os.listdir(dir)
     images = []
@@ -15,5 +24,10 @@ def get_edges(images):
 
 if __name__ == '__main__':
     images = load_images()
+
     edges = get_edges(images)
-    
+
+    pts = np.argwhere(edges[0]==255).reshape(-1,1,2).astype(np.float32)
+    gray0 = cv2.cvtColor(images[0], cv2.COLOR_BGR2GRAY)
+    gray1 = cv2.cvtColor(images[1], cv2.COLOR_BGR2GRAY)
+    p1, st, err = cv2.calcOpticalFlowPyrLK(gray0, gray1, pts, None, **lk_params)
