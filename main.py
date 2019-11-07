691f5e9542eaeb8e9ce15f24e8d5a4f7fcae1262
silvercodex <cody.harmon90@gmail.com>
2019-11-07 09:25:57 -0600

    la

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
-from image_occlusion import *
+import cv2
+import numpy as np
+
 
 
 lk_params = dict( winSize  = (15,15),
@@ -9,8 +11,7 @@ feature_params = dict( maxCorners = 100,
                        qualityLevel = 0.3,
                        minDistance = 7,
                        blockSize = 7 )
-
-p0 = cv2.goodFeaturesToTrack(gray0, mask = None, **feature_params)
+path = "images"
 def load_images(dir = ""):
     files = os.listdir(dir)
     images = []
@@ -23,11 +24,30 @@ def get_edges(images):
 
 
 if __name__ == '__main__':
-    images = load_images()
+    images = load_images(path)
 
     edges = get_edges(images)
-
+    
     pts = np.argwhere(edges[0]==255).reshape(-1,1,2).astype(np.float32)
     gray0 = cv2.cvtColor(images[0], cv2.COLOR_BGR2GRAY)
     gray1 = cv2.cvtColor(images[1], cv2.COLOR_BGR2GRAY)
+    #pts = cv2.goodFeaturesToTrack(edges[0], mask = None, **feature_params)
+    
     p1, st, err = cv2.calcOpticalFlowPyrLK(gray0, gray1, pts, None, **lk_params)
+
+
+    p0 = cv2.goodFeaturesToTrack(gray0, mask = None, **feature_params)
+    p0 = pts
+    color = np.random.randint(0,255,(len(p1),3)) 
+    good_new = p1[st==1]
+    good_old = p0[st==1]
+    frame = images[1]
+    mask = np.zeros_like(frame)
+    
+    for i,(new,old) in enumerate(zip(good_new, good_old)):
+        a,b = new.ravel()
+        c,d = old.ravel()
+        mask = cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
+        frame = cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
+    img = cv2.add(frame,mask)
+    cv2.imwrite("img.png",img)
